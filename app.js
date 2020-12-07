0f9f79e929403b34a093a8382a8ae8b038656cd3
Vineeth <vineeth0906@gmail.com>
2020-12-07 11:10:30 -0500

    new changes in app.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('First Project in cloud computing by vineeth'));
+app.get('/', (req, res) => res.send('Hello Vineeth Goshika'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
